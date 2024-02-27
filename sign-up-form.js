78f44965688fe2516c4d96c2ff07d7d1e70193ec
AnotherUser9898 <anotheruser9898@gmail.com>
2024-02-27 13:19:31 +0530

    Complete Sign up form

---
+++ sign-up-form.js
@@ -0,0 +1,24 @@
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const passwordInputs = document.querySelectorAll(".error");
+const passwordAlert = document.querySelector(".checkPassword");
+
+function checkPassword(mutations) {
+
+}
+passwordInputs.forEach((element) => {
+        element.addEventListener("input",() => {
+                console.log(passwordInput.value);
+                if (passwordInput.value != confirmPasswordInput.value) {
+                        passwordInput.style.cssText += "outline: 2px solid red";
+                        confirmPasswordInput.style.cssText += "outline: 2px solid red";
+                        passwordAlert.textContent = "*Passwords do not match";
+                }
+                else {
+                        passwordInput.style.cssText += "outline: 2px solid royalblue";
+                        confirmPasswordInput.style.cssText += "outline: 2px solid royalblue";
+                        passwordAlert.textContent = "";
+                }
+        })
+
+});
